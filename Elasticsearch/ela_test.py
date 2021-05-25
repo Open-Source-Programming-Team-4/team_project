84fdf15bd0e4e021a67fae57434f1f34bb6379f0
K-Minsu <79684170+K-Minsu@users.noreply.github.com>
2021-05-25 20:54:23 +0900

    Update ela_test.py

--- Elasticsearch/ela_test.py
+++ Elasticsearch/ela_test.py
@@ -8,6 +8,7 @@ es_port = 9200
 '''
 client 생성 >> host 정보 입력
 data 검색 >> search() method arguemnt 에 index 이름, query 내용 입력
+print 결과 >> query 정보 출력
 '''
 if __name__ == '__main__':
   client = Elasticsearch(
