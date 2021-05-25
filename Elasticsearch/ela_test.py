1b6caecdaa95c6277cdc93e7fc2b9884ee27f024
K-Minsu <79684170+K-Minsu@users.noreply.github.com>
2021-05-25 20:52:09 +0900

    Create ela_test.py

---
+++ Elasticsearch/ela_test.py
@@ -0,0 +1,24 @@
+# Elasticsearch 기본 폼
+from elasticsearhch import Elasticsearch
+
+# host, port 설정
+es_host = '127.0.0.1'
+es_port = 9200
+
+'''
+client 생성 >> host 정보 입력
+data 검색 >> search() method arguemnt 에 index 이름, query 내용 입력
+'''
+if __name__ == '__main__':
+  client = Elasticsearch(
+    hosts = [{host : es_host, port : es_port}]
+  )
+  index_name = 'korean_thesis'
+  print(client.search(
+    index = index_name,
+    body = {
+      "query" : {
+        "match_all" : {}
+      }
+    }
+  ))
