fc699f384690ccd9fc76ce091a1f55daa30cedff
K-Minsu <minsue9608@naver.com>
2021-06-19 09:56:54 +0900

    add mod_pkg

---
+++ mod_pkg/ela_search.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+
+from elasticsearch import Elasticsearch
+import crawling
+import crawling_de
+
+'''
+*** elastic search module ***
+
+$ cd elasticsearch-7.6.2
+$ ./bin/elasticsearch
+
+Note : import crawling module "crawling.py" in current directory
+'''
+
+# Elasticsearch 설정
+es_host = '127.0.0.1'
+es_port = 9200
+es = Elasticsearch(hosts = [{"host" : es_host, "port" : es_port}])
+
+'''
+data_set = {
+	"site" : [],		( 사이트 )
+	"title" : [],		( 제목 )
+	"field" : [],		( 분야 )
+	"host" : [],		( 주최사 )
+	"Dday" : [],		( 남은 기간 )
+	"dday-ing" : [],	( 진행상황 )
+	"url" : []		( 링크 )
+}
+'''
+
+# wevity 모든 분야 / 접수 중 데이터 전체 가져오기
+#wevity_data_ing = crawling.CrawlingByField_wevity(2, "end")
+
+# 대티즌 크롤링 데이터 가져오기
+detizen_data = crawling_de.crawling_detizen(10)
+
+# 데이터 저장
+def data_store(idx):
+	''' 접수 중 데이터 저장 '''
+	for i in range(len(detizen_data["site"])):
+		input_data = {
+			"title" : detizen_data["title"][i],
+			"field" : detizen_data["field"][i],
+			"host" : detizen_data["host"][i],
+			"Dday" : detizen_data["Dday"][i],
+			"dday-ing" : detizen_data["dday-ing"][i],
+			"url" : detizen_data["url"][i]
+		}
+		res = es.index(index=idx, doc_type="_doc", id=i+1, body=input_data)
+		print(res)
+
+# 저장 된 전체 데이터 검색
+def data_search_all():
+	query = {
+		"query":{
+			"match_all":{}
+		}
+	}
+	res = es.search(index="ing", body=query, size=10000)
+	print("전체 검색 결과")
+	for hit in res["hits"]["hits"]:
+		print(hit["_source"])
+
+# 제목으로 검색
+'''
+title로 검색을 할 때, input 값과 일치하는 값을 결과로 리턴
+현재 input 값과 일치하는 값 뿐만 아니라, input 값의 단어 중 하나만 포함해도 검색 결과가 리턴되는 오류가 있음
+'''
+def data_search(idx, input_str):
+	query = {
+		"query":{
+			"multi_match":{
+				"query":input_str
+			}
+		}
+	}
+	res = es.search(index=idx, body=query, size=10000)
+	print("검색 결과")
+	for hit in res["hits"]["hits"]:
+		print(hit["_source"])
+
+''' 데이터 저장 & 검색 테스트 '''
+if __name__ == "__main__" :
+	idx = "detizen_idx"
+
+	data_store(idx)
+	data_search_all()
+#	data_search(idx, "2020 부산국제광고제 청소년 크리에이티브 공모전")
