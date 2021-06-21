761d995ee20d831a8e33c3e53132f756ad9dd765
jjwon0318 <71744616+jjwon0318@users.noreply.github.com>
2021-06-22 01:22:31 +0900

    Update crawling.py

--- mod_pkg/crawling.py
+++ mod_pkg/crawling.py
@@ -23,7 +23,7 @@ def Crawling_wevity( P_num ) :
 	    whole_source = whole_source + response.text
         
     # 출력값 - 이름 , 분야 , 주최자 정보를 list로 가지는 dict
-    result = { "site" : [] , "title" : [] , "field" : [] , "host" : [] , "Dday" : [] , "dday-ing" : [] ,"url" : [] }
+    result = { "site" : [] , "title" : [] , "field" : [] , "host" : [] , "Dday" : [] , "dday_ing" : [] ,"url" : [] }
 
     #통합된 html에서 공모전 이름 , 분야 , 주최자 정보 추출 
     soup = BeautifulSoup(whole_source, 'html.parser')
@@ -40,7 +40,7 @@ def Crawling_wevity( P_num ) :
         result["title"].append(item.select_one(".tit > a").text)
         result["field"].append(item.select_one(".tit > .sub-tit").text[5:])
         result["host"].append(item.select_one(".organ").text)
-        result["dday-ing"].append(item.select_one(".day > span").text)
+        result["dday_ing"].append(item.select_one(".day > span").text)
         item.select_one(".day > span").decompose()
         result["Dday"].append(item.select_one(".day").text.strip())
         result["url"].append("https://www.wevity.com/" + item.select_one(".tit > a")["href"])
@@ -67,7 +67,7 @@ if __name__ == "__main__" :
         print(data["field"][i])
         print(data["host"][i])
         print(data["Dday"][i])
-        print(data["dday-ing"][i])
+        print(data["dday_ing"][i])
         print(data["url"][i])
         print("----------------------------------------")
     print(str(len(data["title"]))+"개의 공모전 정보를 탐색했습니다.")
