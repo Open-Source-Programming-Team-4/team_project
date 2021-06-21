51fef330620c074a6970ffd24a39a35989b6428d
Kangmin Han <62194473+hankn01@users.noreply.github.com>
2021-06-21 20:26:52 +0900

    Create Webinit.py

---
+++ mod_pkg/Webinit.py
@@ -0,0 +1,17 @@
+  
+#!/usr/bin/python3
+
+
+def search(searchobject, case):
+  idx = "data_idx"
+        # "title" / "field" / "host" 검색 설정
+  dt = data_search_cs(case, idx, searchobject)
+  
+  return dt
+if __name__=='__main__':
+    keyword = input()
+    res =  search(keyword)
+    print(res['Dday'])
+    from ela_search import *
+else:
+    from mod_pkg.ela_search import *
